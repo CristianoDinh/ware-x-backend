a63e72a12991c6d349ee2ae6285d4c6bc6e1e36c
CristianoDinh <dinhgiabao1142004@gmail.com>
2025-06-03 04:35:53 +0700

    add backend SpringBoot project

---
+++ src/main/java/com/baobao/ware_x/WareXApplication.java
@@ -0,0 +1,13 @@
+package com.baobao.ware_x;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WareXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WareXApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baobao/ware_x/WareXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baobao.ware_x;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WareXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
